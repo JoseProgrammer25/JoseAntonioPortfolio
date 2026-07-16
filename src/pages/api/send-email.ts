9e492c5601b5c65945f81b9178c2521f5724569d
JoseProgrammer25 <joseprogrammerdeveloper@gmail.com>
2026-07-16 15:03:00 +0200

    funcion de mandar correos completa

---
+++ src/pages/api/send-email.ts
@@ -0,0 +1,41 @@
+export const prerender = false;
+import type { APIRoute } from "astro";
+import { BrevoClient } from "@getbrevo/brevo";
+
+export const POST: APIRoute = async ({ request }) => {
+  const data = await request.json();
+  const { name, email, message } = data;
+
+  const brevo = new BrevoClient({
+    apiKey: import.meta.env.BREVO_API_KEY,
+  });
+
+  try {
+    await brevo.transactionalEmails.sendTransacEmail({
+      subject: `Nuevo mensaje de: ${name}`,
+      sender: {
+        name: "Portfolio",
+        email: "joseprogrammerdeveloper@gmail.com",
+      },
+      to: [{ email: "jabecerramorilla21@gmail.com" }],
+
+      htmlContent: `
+        <p><strong>De:</strong> ${name} (${email})</p>
+        <p><strong>Mensaje:</strong></p>
+        <p>${message}</p>
+      `,
+    });
+
+    return new Response(
+      JSON.stringify({ message: "Correo enviado con éxito" }),
+      {
+        status: 200,
+      },
+    );
+  } catch (error) {
+    console.error("Error al enviar correo:", error);
+    return new Response(JSON.stringify({ message: "Error al enviar" }), {
+      status: 500,
+    });
+  }
+};
